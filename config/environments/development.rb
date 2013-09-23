9787ba94ff0f3dd49e176df2ec37172da0e3dd07
Justin Tanacredi <Jtanacredi@gmail.com>
2013-09-22 23:24:12 -0400

    Create Admin Login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = { 
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "domain.of.sender.net",
+  authentication: "plain",
+  user_name: "jtanacredi", 
+  password: "patrickrocks", 
+  enable_starttls_auto: true
+}
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -14,6 +23,7 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
